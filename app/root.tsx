1e7b2d570fc8880cd812a63f60f488d40e515363
Kent C. Dodds <me+github@kentcdodds.com>
2022-09-08 10:10:57 -0600

    fix things

--- app/root.tsx
+++ app/root.tsx
@@ -18,7 +18,7 @@ export const links: LinksFunction = () => {
 
 export const meta: MetaFunction = () => ({
 	charset: 'utf-8',
-	title: 'Remix Notes',
+	title: 'Rocket Rental',
 	viewport: 'width=device-width,initial-scale=1',
 })
 
