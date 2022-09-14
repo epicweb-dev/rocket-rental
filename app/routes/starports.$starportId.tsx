e6639afa6ec378a00bc66efa5229a6e072dbfd30
Kent C. Dodds <me+github@kentcdodds.com>
2022-09-14 14:01:30 -0600

    flat routes

---
+++ app/routes/starports.$starportId.tsx
@@ -0,0 +1,3 @@
+export default function UnnamedRoute() {
+	return <div>UnnamedRoute</div>
+}
--- remix.config.js
+++ remix.config.js
@@ -1,7 +1,12 @@
+const { flatRoutes } = require('remix-flat-routes')
+
 /**
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
 	cacheDirectory: './node_modules/.cache/remix',
-	ignoredRouteFiles: ['**/.*', '**/*.css', '**/*.test.{js,jsx,ts,tsx}'],
+	ignoredRouteFiles: ['**/*'],
+	routes: async defineRoutes => {
+		return flatRoutes('routes', defineRoutes)
+	},
 }
