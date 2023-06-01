14fbbbbb8b861f44fcd0266b51130e01a4ee63f8
onemen <tabmix.onemen@gmail.com>
2023-06-01 23:01:27 +0300

    update unused/missing dependencies

--- other/build-server.ts
+++ other/build-server.ts
@@ -1,7 +1,7 @@
 import fsExtra from 'fs-extra'
 import path from 'path'
 import { fileURLToPath } from 'url'
-import glob from 'glob'
+import { globSync } from 'glob'
 import esbuild from 'esbuild'
 
 const pkg = fsExtra.readJsonSync(path.join(process.cwd(), 'package.json'))
@@ -9,7 +9,7 @@ const pkg = fsExtra.readJsonSync(path.join(process.cwd(), 'package.json'))
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 const here = (...s: Array<string>) => path.join(__dirname, ...s)
 
-const allFiles = glob.sync(here('../server/**/*.*'), {
+const allFiles = globSync(here('../server/**/*.*'), {
 	ignore: ['**/tsconfig.json', '**/eslint*', '**/__tests__/**'],
 })
 
@@ -30,7 +30,7 @@ console.log('building...')
 
 esbuild
 	.build({
-		entryPoints: glob.sync(here('../server/**/*.+(ts|js|tsx|jsx)')),
+		entryPoints: globSync(here('../server/**/*.+(ts|js|tsx|jsx)')),
 		outdir: here('../server-build'),
 		target: [`node${pkg.engines.node}`],
 		platform: 'node',
